cd413ca3a93b0efd57f60f94a2ece9d1475868ea
pra6hj0t <157087592+pra6hj0t@users.noreply.github.com>
2024-05-19 15:58:23 +0530

    State With Switch

--- app/src/main/java/com/example/toolbar/MainActivity.kt
+++ app/src/main/java/com/example/toolbar/MainActivity.kt
@@ -7,7 +7,9 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -19,12 +21,18 @@ import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.toolbar.ui.theme.ToolbarTheme
 
 class MainActivity : ComponentActivity() {
@@ -74,7 +82,12 @@ class MainActivity : ComponentActivity() {
                            verticalArrangement = Arrangement.Center
                        ){
                            Text(text = "Hello Prabh Toolbar is Working")
+
+                           Spacer(modifier = Modifier.height(20.dp))
+
+                           showSwitch()
                        }
+
                 }
             }
         }
@@ -82,17 +95,12 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun showSwitch(){
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ToolbarTheme {
-        Greeting("Android")
+    var isChecked = remember {
+        mutableStateOf(false)
     }
+    Switch(checked =isChecked.value, onCheckedChange ={
+        isChecked.value = it
+    })
 }
